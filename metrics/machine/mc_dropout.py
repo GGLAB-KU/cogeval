d162ef79ce884445d80b8f99930b7043dd95e88c
mg9 <mugekural@gmail.com>
2022-10-28 15:23:57 +0300

    metric configs wip

--- metrics/machine/mc_dropout.py
+++ metrics/machine/mc_dropout.py
@@ -1,12 +1,13 @@
 
 import numpy as np
 import pandas as pd
+import yaml, argparse, os
+from yaml.loader import SafeLoader
 from scipy.special import softmax
 from scipy.stats import entropy
 from common import *
 
 def mc_dropout(data, model, tokenizer, iters):
-  num_classes = 3
   sm_sum = dict()
   for counter in range(iters):
       print('Iteration: ', counter)
@@ -14,13 +15,14 @@ def mc_dropout(data, model, tokenizer, iters):
       for ind, row in data.iterrows():
           text = row['content']
           encoded_input = tokenizer(text, return_tensors='pt')
+          encoded_input = encoded_input.to('cuda')
           output = model(**encoded_input)
-          scores = output[0][0].detach().numpy()
+          scores = output[0][0].detach().cpu() #.numpy()
           scores = softmax(scores)
           pred = np.argmax(scores)
           guid = row['sstb_id'] 
           if guid in sm_sum:
-              for i in range(num_classes):
+              for i in range(len(scores)):
                 sm_sum[guid][i] += scores[i]
           else:
             sm_sum[guid] = scores.copy()
@@ -28,25 +30,72 @@ def mc_dropout(data, model, tokenizer, iters):
             correct += 1
   return sm_sum
 
-def entropy_over_softmax(df, probs, iters):
-  sample_entropy = dict()
+
+def get_metrics(df, probs, iters):
+  pred_confidences = dict()
+  gold_confidences = dict()
+  uncertainty =  dict()
+
   out_df =  pd.DataFrame({})
   for guid in probs.keys():
+    
     # avg over iterations 
     probs[guid] = probs[guid] / iters
-    sample_entropy[guid] = entropy(probs[guid], base=2)
+
+    # get confidence score of model's prediction
+    pred_confidences[guid] = max(probs[guid])
+    
+    # get uncertainty over model's prediction
+    uncertainty[guid] = entropy(probs[guid], base=2)
+
     # append entropy values to the data
     guid_row = df.loc[df['sstb_id'] == guid]
+
+    # get confidence score for ground truth
+    gold_label = guid_row.iloc[0]['sstb_label']
+    gold_confidences[guid] = probs[guid][gold_label]
+
     df2 = guid_row[['sstb_id','zuco_id','content','control_id','sstb_label']]
-    df2['BERT_MC_DROPOUT'] = sample_entropy[guid]
+    df2['pred_confidence'] = pred_confidences[guid]
+    df2['gold_confidence'] = gold_confidences[guid]
+    df2['uncertainty'] = uncertainty[guid]
+
     out_df = pd.concat([out_df, df2])  
   return out_df
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file')
+    args = parser.parse_args()
+    
+    with open(args.config_file) as f:
+        config = yaml.load(f, Loader=SafeLoader)
+    
+    # load data
+    data_path      = config['data']['file'] 
+    data = load_data(data_path)
+
+    # inference with model after calibration with mc_dropout
+    model_name     = config['model']['name']
+    num_iters      = config['model']['calibration']['num_iters']
+    model, tokenizer = load_model(model_name)
+    model = model.to('cuda')
+
+    sft_values = mc_dropout(data, model, tokenizer, num_iters)
+    results   = get_metrics(data, sft_values, num_iters)
+  
+  
+    # save metric results
+    results_dir  = config['results']['dir']
+    results_file  = config['results']['file']
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    results.to_csv(results_dir+'/'+results_file)
+
 if __name__=="__main__":
-  iters = 50
-  data = load_data('data/human/sentiment-analysis/all.csv')
-  model, tokenizer = load_model('Souvikcmsa/BERT_sentiment_analysis')
-  sft_values = mc_dropout(data, model, tokenizer, iters)
-  results   = entropy_over_softmax(data, sft_values, iters)
-  results.to_csv('results/sentiment-analysis/mc_dropout.csv', index=False)
+    # specify parameters in .yaml file under ./config/metrics directory 
+    # usage: python mc_dropout.py <config_file>
+    # e.g. : python mc_dropout.py config/metrics/sa_all.yaml
+    main()
+    
